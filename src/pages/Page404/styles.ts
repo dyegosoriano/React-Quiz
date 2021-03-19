7642ff026255ad7f815c96247e37063f18e3f0c9
Dyego Soriano <dyegodf2@gmail.com>
2021-03-19 15:46:32 -0300

    estilizando layout da Page404

--- src/pages/Page404/styles.ts
+++ src/pages/Page404/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-size: 4rem;
+  }
+`
