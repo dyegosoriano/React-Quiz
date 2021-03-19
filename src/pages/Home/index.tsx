a69180ca5deac5916e812d45be827d360312a940
Dyego Soriano <dyegodf2@gmail.com>
2021-03-19 12:46:31 -0300

    estilizando layout da página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 
 import { Container } from './styles'
 
 export default function Home(): JSX.Element {
+  const history = useHistory()
+
   return (
     <Container>
-      <h1>Home</h1>
+      <header>
+        <h1>!!!WELCOME TO QUIZ!!!</h1>
+      </header>
+
+      <main>
+        <p>10 questions will be shown to you.</p>
+        <p>Can you get maximum score?</p>
+      </main>
+
+      <footer>
+        <button onClick={() => history.push('/questions')}>START</button>
+      </footer>
     </Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,35 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 100px 1fr 100px;
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    p {
+      text-align: center;
+      font-size: 2rem;
+    }
+  }
+
+  header,
+  footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    button {
+      background: #ff6f00;
+      color: #fff;
+
+      padding: 8px;
+      border: none;
+      border-radius: 4px;
+    }
+  }
+`
