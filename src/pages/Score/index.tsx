f9091abb75629b3027e8461d086b30a129195e8b
Dyego Soriano <dyegodf2@gmail.com>
2021-03-19 15:40:07 -0300

    estilizando layout da página Score

--- src/pages/Score/index.tsx
+++ src/pages/Score/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { MdDone, MdClear } from 'react-icons/md'
+import { useHistory } from 'react-router'
 
 import { Container } from './styles'
 
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+
 export default function Score(): JSX.Element {
+  const history = useHistory()
+
   return (
     <Container>
-      <h1>Score</h1>
+      <Header title="You score: 30%" />
+
+      <ul>
+        <li>
+          <MdDone size={24} color="#1b5e20" />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus facilis
+          ea velit adipisci quidem, eius similique debitis modi perferendis,
+          harum deleniti consectetur vel officia dolor ullam aliquam. Nostrum,
+          libero veniam?
+        </li>
+        <li>
+          <MdClear size={24} color="#d50000" />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus facilis
+          ea velit adipisci quidem, eius similique debitis modi perferendis,
+          harum deleniti consectetur vel officia dolor ullam aliquam. Nostrum,
+          libero veniam?
+        </li>
+      </ul>
+
+      <Footer>
+        <button onClick={() => history.push('/')}>!START AGAIN!</button>
+      </Footer>
     </Container>
   )
 }
--- src/pages/Score/styles.ts
+++ src/pages/Score/styles.ts
@@ -1,3 +1,25 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 100px 1fr 100px;
+
+  ul {
+    max-height: calc(100vh - 200px);
+    overflow-y: scroll;
+
+    li {
+      margin: 0 16px 16px 16px;
+      list-style-type: none;
+      text-align: justify;
+
+      svg {
+        margin-right: 6px;
+        position: relative;
+        top: 6px;
+      }
+    }
+  }
+`
