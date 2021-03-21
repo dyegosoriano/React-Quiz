a00fbb75d1318af30172fcd00a473bbbf54f2ab8
Dyego Soriano <dyegodf2@gmail.com>
2021-03-21 13:34:32 -0300

    buscando dados da API com axios e salvando na QuestionsContext

--- src/context/QuestionsContext.tsx
+++ src/context/QuestionsContext.tsx
@@ -1,7 +1,16 @@
 import React, { createContext, useEffect, useState } from 'react'
+import api from 'services/api'
 
 interface contextInterface {
+  questions: questionsInterface[]
   score: number
+}
+
+interface questionsInterface {
+  correct_answer: string
+  difficulty: string
+  question: string
+  category: string
 }
 
 export const QuestionsContext = createContext<contextInterface>(
@@ -9,12 +18,29 @@ export const QuestionsContext = createContext<contextInterface>(
 )
 
 export const QuestionsProvider: React.FC = ({ children }) => {
+  const [questions, setQuestions] = useState<questionsInterface[]>([])
   const [score, setScore] = useState<number>(0)
 
-  useEffect(() => setScore(17), [])
+  async function getQuestions(): Promise<void> {
+    try {
+      const response = await api.get(
+        'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
+      )
+
+      setQuestions(response.data.results)
+    } catch (error) {
+      console.log(error.message)
+      alert('Ocorreu um erro ao carregar os dados da API.')
+    }
+  }
+
+  useEffect(() => {
+    getQuestions()
+    setScore(17)
+  }, [])
 
   return (
-    <QuestionsContext.Provider value={{ score }}>
+    <QuestionsContext.Provider value={{ questions, score }}>
       {children}
     </QuestionsContext.Provider>
   )
---
+++ src/services/api.ts
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
+})
+
+export default api
