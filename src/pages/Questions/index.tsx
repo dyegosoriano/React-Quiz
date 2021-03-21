70431c32f409bcbdd4331059abf5858cb9adee6b
Dyego Soriano <dyegodf2@gmail.com>
2021-03-21 14:26:28 -0300

    alterando questões da página Questions

--- src/pages/Questions/index.tsx
+++ src/pages/Questions/index.tsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useHistory } from 'react-router'
 
 import { Container } from './styles'
 
+import { QuestionsContext } from 'context/QuestionsContext'
 import Header from 'components/Header'
 
 export default function Questions(): JSX.Element {
+  const { questions } = useContext(QuestionsContext)
+  const history = useHistory()
+
+  const [counter, setCounter] = useState(0)
+
+  function handleNextQuestion(): void {
+    if (counter < questions.length - 1) {
+      setCounter(counter + 1)
+    } else {
+      history.push('/score')
+    }
+  }
+
   return (
     <Container>
       <Header title="General Knowledge" />
 
       <main>
-        <span>
-          The surface area of Russia is slightly larger than that of the dwarf
-          planet Pluto.
-        </span>
+        <span>{questions[counter].question}</span>
 
         <div className="box__options">
-          <button>TRUE</button>
-          <button>FALSE</button>
+          <button onClick={handleNextQuestion}>TRUE</button>
+          <button onClick={handleNextQuestion}>FALSE</button>
         </div>
       </main>
     </Container>
